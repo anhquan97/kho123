e51aeb4528b2b07f1d68c1cce303c76687ceb979
anhquan97 <44152493+anhquan97@users.noreply.github.com>
2018-10-19 11:05:12 +0700

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,3 +1,27 @@
-int USCLN(int a, int b)
+#include<math.h>
+int uCLN(int a, int b)
 {
+	a = abs(a); // trị tuyệt đối cho số âm
+	b = abs(b);  // trị tuyệt đối cho số âm
+	if (a == 0 && b != 0)
+	{
+		return b;
+	}
+	else if (a != 0 && b == 0)
+	{
+		return a;
+	}
+	while (a != b)
+	{
+		if (a > b)
+		{
+			a -= b;
+		}
+		else
+		{
+			b -= a;
+		}
+	}
+	return a;
+
 }
